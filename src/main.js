f4abc0ed5b77ca5779b26de78e4d35112874cecb
伪淑女 <5642012+fake_lady@user.noreply.gitee.com>
2020-01-20 10:18:32 +0800

    底部tabbar

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import { Header } from 'mint-ui'
 import app from './App.vue'
 import './lib/mui/css/mui.css'
+import "./lib/mui/css/icons-extra.css"
 
 Vue.component(Header.name, Header);
 var vm = new Vue({
